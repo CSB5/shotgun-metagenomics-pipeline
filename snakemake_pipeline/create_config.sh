40771cc0ae2543b205c91d9058da554121397d2d
Chenhao Li <lch14forever@gmail.com>
2016-06-23 11:04:02 +0800

    config file now contains the tools and dbs

--- snakemake_pipeline/create_config.sh
+++ snakemake_pipeline/create_config.sh
@@ -2,4 +2,33 @@
 ## $PATH_TO_THIS_FILE/create_config.sh $PATH_TO_YOUR_LIB/*h5
 ## create a config file "conf.yaml"
 
-{ echo "SAMPLES:"; ls -d1 $@ | sed 's/\/$//' | awk -F/ '{printf "  %s: %s\n", $NF, $0}'; } | sed 's/.h5:/:/' > conf.yaml
+## default tools
+cat <<EOT > conf.yaml
+TOOLS:
+  FAMAS: /mnt/software/stow/famas-0.0.10/bin/famas
+  BWA: /mnt/software/stow/bwa-0.7.10/bin/bwa
+  METAPHLAN2: /mnt/projects/lich/dream_challenge/tmp/biobakery-metaphlan2-3d22a72b109b/metaphlan2.py
+  METAPHLAN: /mnt/software/stow/metaphlan-2f1b17a1f4e9/bin/metaphlan.py
+  DECONT: /mnt/software/stow/decont-0.3/bin/decont.py
+EOT
+
+## default DBs
+cat <<EOD >> conf.yaml
+DBS:
+  DECONT_GENOME: /mnt/genomeDB/genomeIndices/hg19/bwa_index/nucleotide/hg19.fa
+  METAPHLAN_BTDB: /mnt/genomeDB/misc/softwareDB/metaphlan/huttenhower.sph.harvard.edu/metaphlan/bowtie2db/mpa
+  METAPHLAN2_PKL: /mnt/projects/lich/dream_challenge/tmp/biobakery-metaphlan2-3d22a72b109b/db_v20/mpa_v20_m200.pkl
+  METAPHLAN2_DB: /mnt/projects/lich/dream_challenge/tmp/biobakery-metaphlan2-3d22a72b109b/db_v20/mpa_v20_m200
+  KRAKEN_DB: /mnt/genomeDB/misc/softwareDB/kraken/minikraken_20141208/
+EOD
+
+## default profilers to run
+cat <<EOP >> conf.yaml
+PROFILERS:
+  metaphlan: 1
+  kraken: 1
+  metaphlan2: 1
+EOP
+
+
+{ echo "SAMPLES:"; ls -d1 "$@" | sed 's/\/$//' | awk -F/ '{printf "  %s: %s\n", $NF, $0}'; } | sed 's/.h5:/:/' >> conf.yaml
--- snakemake_pipeline/run_pipeline.sh
+++ snakemake_pipeline/run_pipeline.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-TOTAL_THREADS=${1:-24}
+if [[ $1 =~ -.* ]] || [ -z $1 ] ;then
+    TOTAL_THREADS=24
+    ARGS="$@"
+else
+    TOTAL_THREADS=$1
+    ARGS="${@:2}"
+fi
 
-/mnt/projects/rpd/apps.testing/miniconda3/envs/snakemake-3.7.1/bin/snakemake  --configfile conf.yaml -p -T --snakefile ~lich/projects_backup/metagenomics_pipeline/snakemake_pipeline/Snakefile -j $TOTAL_THREADS --drmaa ' -pe OpenMP {threads} -l {params.resource} -V -b y -cwd -w n'
+
+/mnt/projects/rpd/apps.testing/miniconda3/envs/snakemake-3.7.1/bin/snakemake  --configfile conf.yaml -p -T --snakefile ~lich/projects_backup/metagenomics_pipeline/snakemake_pipeline/Snakefile -j $TOTAL_THREADS --drmaa ' -pe OpenMP {threads} -l {params.resource} -V -b y -cwd -w n' $ARGS
